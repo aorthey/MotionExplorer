cb0dcca2318d51b0ca05cac2787c66dc77756ef5
Andreas Orthey <andreas.orthey@gmx.de>
2016-12-30 15:35:33 +0900

    minor

--- main.cpp
+++ main.cpp
@@ -8,50 +8,70 @@
 #include <stdio.h>
 #include "src/gui.h"
 #include "src/info.h"
+#include "src/object.h"
 
 
 int main(int argc,const char** argv) {
-        RobotWorld world;
+  RobotWorld world;
 
-        ForceFieldBackend backend(&world);
-        //SimTestBackend backend(&world);
-        WorldSimulation& sim=backend.sim;
+  ForceFieldBackend backend(&world);
+  //SimTestBackend backend(&world);
+  WorldSimulation& sim=backend.sim;
 
-        //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hubo_fractal_3.xml");
-        backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/athlete_fractal_1.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hubo_fractal_3.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/athlete_fractal_1.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/orthoklampt/data/hubo_object.xml");
+  backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hubo_pushdoor.xml");
 
-        Info info(&world);
+  Info info(&world);
+  info.print();
 
-        info.print();
 
-        GLUISimTestGUI gui(&backend,&world);
-        //ForceFieldGUI gui(&backend,&world);
-        gui.SetWindowTitle("SimTest2");
-        gui.Run();
+  //always load objects over backend, not in world! (backend takes care of
+  //associating it with frontend gui)
 
-        // while(1) {
-        //         //sim.Advance(dt);
-        //         //sim.UpdateModel();
-        //         cout<<sim.time<<'\t'<<world.robots[0]->q<<endl;
-        // }
-        ////run the simulation
-        //while(sim.time < 5) {
-        //        backend.RenderWorld();
-        //        if(sim.time >= 2.0 && sim.time-dt < 2.0) {
-        //                Config q;
-        //                sim.robotControllers[0]->GetCommandedConfig(q);
-        //                q[7] -= 1.0;
-        //                //LexicalCast is needed to convert config to string
-        //                sim.robotControllers[0]->SendCommand("set_q",LexicalCast(q));
-        //                //then move link 7 (hip pitch) 1.5 radians down
-        //                q[7] += 1.5;
-        //                sim.robotControllers[0]->SendCommand("append_q",LexicalCast(q));
-        //        }
-        //        sim.Advance(dt);
-        //        sim.UpdateModel();
-        //        cout<<sim.time<<'\t'<<world.robots[0]->q<<endl;
-        //        backend.DoStateLogging_LinearPath(0,"test_state.path");
-        //}
-        return 0;
+  //string objFile = "/home/aorthey/git/Klampt/data/robots/door.rob";
+  //backend.LoadFile(objFile.c_str());
+  //string cubeFile = "/home/aorthey/git/Klampt/data/robots/free_cube.rob";
+  //backend.LoadFile(cubeFile.c_str());
+
+  //Robot *cube = world.GetRobot("free_cube");
+  //Config q = cube->q;
+  //std::cout << q << std::endl;
+  //q[0] = 1.0;
+  //q[1] = 1.0;
+  //q[2] = 0.5;
+  //cube->UpdateConfig(q);
+  //ObjectPlacementManager objectified(&world);
+  //objectified.spawn();
+
+  GLUISimTestGUI gui(&backend,&world);
+  gui.SetWindowTitle("SimTest2");
+  gui.Run();
+
+  // while(1) {
+  //         //sim.Advance(dt);
+  //         //sim.UpdateModel();
+  //         cout<<sim.time<<'\t'<<world.robots[0]->q<<endl;
+  // }
+  ////run the simulation
+  //while(sim.time < 5) {
+  //        backend.RenderWorld();
+  //        if(sim.time >= 2.0 && sim.time-dt < 2.0) {
+  //                Config q;
+  //                sim.robotControllers[0]->GetCommandedConfig(q);
+  //                q[7] -= 1.0;
+  //                //LexicalCast is needed to convert config to string
+  //                sim.robotControllers[0]->SendCommand("set_q",LexicalCast(q));
+  //                //then move link 7 (hip pitch) 1.5 radians down
+  //                q[7] += 1.5;
+  //                sim.robotControllers[0]->SendCommand("append_q",LexicalCast(q));
+  //        }
+  //        sim.Advance(dt);
+  //        sim.UpdateModel();
+  //        cout<<sim.time<<'\t'<<world.robots[0]->q<<endl;
+  //        backend.DoStateLogging_LinearPath(0,"test_state.path");
+  //}
+  return 0;
 }
 
