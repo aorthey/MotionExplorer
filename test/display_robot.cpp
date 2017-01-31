6ffeb68e2b155f4425d8ecef2d4f4407a4c387c5
Andreas Orthey <andreas.orthey@gmx.de>
2017-01-30 17:01:33 -0800

    seperate display_robot test

---
+++ test/display_robot.cpp
@@ -0,0 +1,24 @@
+#include <Interface/SimTestGUI.h>
+#include <stdio.h>
+#include "info.h"
+
+int main(int argc,const char** argv) {
+  RobotWorld world;
+  SimTestBackend backend(&world);
+  WorldSimulation& sim=backend.sim;
+
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/athlete_fractal_1.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hrp2.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hubo_fractal_2.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/Klampt/data/hubo_fractal_3.xml");
+  //backend.LoadAndInitSim("/home/aorthey/git/orthoklampt/data/hubo_object.xml");
+  backend.LoadAndInitSim("/home/aorthey/git/orthoklampt/data/sentinel2.xml");
+  Info info;
+  info(&world);
+
+  GLUISimTestGUI gui(&backend,&world);
+  gui.SetWindowTitle("SimTest");
+  gui.Run();
+  return 0;
+}
+
