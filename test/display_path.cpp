a9697c00a84e457409721972848147d576837432
Andreas Orthey <andreas.orthey@gmx.de>
2017-02-15 18:48:24 -0800

    load and display a given path

---
+++ test/display_path.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+//#include <Interface/SimulationGUI.h>
+#include <Interface/SimTestGUI.h>
+#include <KrisLibrary/GLdraw/GL.h>
+#include <KrisLibrary/GLdraw/drawextra.h>
+
+#include <Contact/Grasp.h> //class Grasp
+#include <Planning/ContactCSpace.h>
+#include <Modeling/MultiPath.h>
+#include <Planning/PlannerSettings.h>
+#include <KrisLibrary/planning/AnyMotionPlanner.h>
+#include <Modeling/DynamicPath.h>
+#include <Modeling/Paths.h>
+#include <Control/PathController.h>
+
+#include "gui.h"
+#include "info.h"
+#include "planner.h"
+#include "object.h"
+#include "iksolver.h"
+#include "iksolver_hubo.h"
+#include "iksolvergrasp_hubolefthandcylinder.h"
+#include "iksolvergrasp_robonaut.h"
+#include "controller.h"
+
+int main(int argc,const char** argv) {
+  RobotWorld world;
+  Info info;
+  ForceFieldBackend backend(&world);
+  //SimTestBackend backend(&world);
+  WorldSimulation& sim=backend.sim;
+
+  glDisable(GL_CULL_FACE);
+  backend.LoadAndInitSim("/home/aorthey/git/orthoklampt/data/sentinel.xml");
+
+  MultiPath path;
+  path.Load("last_path.xml");
+  backend.VisualizePathSweptVolume(path);
+
+  world.rigidObjects.clear();
+
+
+
+  //############################################################################
+  //guification
+  //############################################################################
+
+  std::cout << "start GUI" << std::endl;
+  GLUISimTestGUI gui(&backend,&world);
+  gui.SetWindowTitle("SweptVolumePath");
+  gui.Run();
+
+  return 0;
+}
+
+
+
