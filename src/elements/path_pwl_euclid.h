e825e8f1435b2e7a6140d4112b85bcbfa7f40fe9
Andreas Orthey <andreas.orthey@gmx.de>
2017-03-31 17:10:41 +0900

    pwl path

---
+++ src/elements/path_pwl_euclid.h
@@ -0,0 +1,119 @@
+#include <Library/KrisLibrary/math/vector.h>
+typedef Vector Config;
+
+// Continuous Piecewise-Linear Path on Euclidean Space without velocity information
+//
+// [0,L] -> R^n, piecewise linear
+// [0,1] -> R^n, piecewise linear (normalized)
+class PathPiecewiseLinearEuclidean
+{
+
+  protected:
+    std::vector<Config> keyframes;
+    uint Ndim;
+    uint Nkeyframes;
+    double length;
+    std::vector<double> interLength;//interLength(i) length towards next milestone point from q(i)
+    PathPiecewiseLinearEuclidean(){};
+
+  public:
+    static PathPiecewiseLinearEuclidean from_keyframes(const std::vector<Config> &keyframes);
+
+    Config EvalMilestone(const int k){
+      if(k<0) return keyframes.front();
+      if(k>Nkeyframes) return keyframes.back();
+      return keyframes.at(k);
+    }
+
+    // make it [0,1] -> R^n
+    void Normalize(){
+      double newLength =0.0;
+      for(int i = 0; i < interLength.size(); i++){
+        interLength.at(i) /= length;
+        newLength+=interLength.at(i);
+
+      }
+      assert( fabs(newLength-1.0) < 1e-10);
+      length = newLength;
+    }
+    std::vector<double> GetLengthVector(){
+      return interLength;
+    }
+
+    double GetLength(){
+      return length;
+    }
+
+    Vector3 EvalVec3(const double t){
+      Config q = Eval(t);
+      Vector3 v; v[0] = q(0); v[1] = q(1); v[2] = q(2);
+      return v;
+    }
+    Config Eval(const double t){
+      if(t<=0) return keyframes.front();
+      if(t>=length) return keyframes.back();
+      uint ictr = 0;
+      double Tcum = 0;
+      assert(interLength.size()==keyframes.size()-1);
+      for(int i = 0; i < interLength.size(); i++){
+        double Tnext = interLength.at(i);
+        if((Tcum+Tnext)>=t){
+          //t \in [Lcum, Lcum+Lnext]
+          double tloc = (t-Tcum)/Tnext; //tloc \in [0,1]
+          Config q1 = keyframes.at(i);
+          Config q2 = keyframes.at(i+1);
+          return q1 + tloc*(q2-q1);
+        }
+        Tcum+=Tnext;
+      }
+      //rounding errors could lead to the fact that the cumulative length is not
+      //exactly 1. If t is sufficiently close, we just return the last keyframe.
+      if(length-Tcum > 1e-10){
+        std::cout << "length of path is significantly different from 1.0" << std::endl;
+        std::cout << "length    : " << Tcum << "/" << 1.0 << std::endl;
+        std::cout << "difference: " << 1.0-Tcum << std::endl;
+        throw;
+      }
+
+      if(t>=Tcum){
+        return keyframes.back();
+      }
+
+      std::cout << "Eval could not find point for value " << t << std::endl;
+      throw;
+    }
+    virtual void interpolate(){
+      length = 0;
+      interLength.clear();
+      for(int i = 0; i < Nkeyframes-1; i++){
+        Config q1 = keyframes.at(i);
+        Config q2 = keyframes.at(i+1);
+        double d = (q1-q2).norm();
+        length += d;
+        interLength.push_back(d);
+      }
+    }
+  
+    virtual void info(){
+      std::cout << std::string(80, '-') << std::endl;
+      std::cout << "Path Length   : " << length << std::endl;
+      std::cout << "Path Keyframes: " << Nkeyframes << std::endl;
+      std::cout << "Path Dimensionality: " << Ndim << std::endl;
+      std::cout << std::string(80, '-') << std::endl;
+      double dstep = length/10.0;
+      for(double d = 0; d < length; d+=dstep){
+        std::cout << Eval(d) << std::endl;  
+      }
+      std::cout << Eval(length) << std::endl;  
+    }
+
+};
+
+inline PathPiecewiseLinearEuclidean PathPiecewiseLinearEuclidean::from_keyframes(const std::vector<Config> &keyframes){
+  PathPiecewiseLinearEuclidean path = PathPiecewiseLinearEuclidean();
+  path.keyframes=keyframes;
+  path.Ndim = keyframes.at(0).size();
+  path.Nkeyframes = keyframes.size();
+  path.interpolate();
+  return path;
+}
