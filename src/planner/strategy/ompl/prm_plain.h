341943744aac6a77d3850779f6786b51abc4992e
Andreas Orthey <andreas.orthey@gmx.de>
2017-12-05 15:05:11 +0900

    using associated vertex

--- src/planner/strategy/ompl/prm_plain.h
+++ src/planner/strategy/ompl/prm_plain.h
@@ -39,10 +39,6 @@ namespace ompl
         {
             typedef boost::vertex_property_tag kind;
         };
-        struct vertex_associated_edge_t
-        {
-            typedef boost::vertex_property_tag kind;
-        };
         struct vertex_associated_vertex_t
         {
             typedef boost::vertex_property_tag kind;
@@ -73,9 +69,8 @@ namespace ompl
                 boost::property<vertex_total_connection_attempts_t, unsigned long int,
                 boost::property<vertex_successful_connection_attempts_t, unsigned long int,
                 boost::property<boost::vertex_predecessor_t, unsigned long int,
-                boost::property<vertex_associated_edge_t, unsigned long int,
                 boost::property<vertex_associated_vertex_t, unsigned long int,
-                boost::property<boost::vertex_rank_t, unsigned long int>>>>>>
+                boost::property<boost::vertex_rank_t, unsigned long int>>>>>
               >,
               boost::property<boost::edge_weight_t, EdgeProperty>
               //boost::property<boost::edge_weight_t, base::Cost>>
@@ -124,7 +119,6 @@ namespace ompl
         boost::property_map<Graph, vertex_state_t>::type stateProperty_;
         boost::property_map<Graph, vertex_total_connection_attempts_t>::type totalConnectionAttemptsProperty_;
         boost::property_map<Graph, vertex_successful_connection_attempts_t>::type successfulConnectionAttemptsProperty_;
-        boost::property_map<Graph, vertex_associated_edge_t>::type associatedEdgeProperty_;
         boost::property_map<Graph, vertex_associated_vertex_t>::type associatedVertexProperty_;
 
         base::OptimizationObjectivePtr opt_;
