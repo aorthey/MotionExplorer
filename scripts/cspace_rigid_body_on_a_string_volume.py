3585a6bc58af86817a81a820dff6eaca80ab62f5
Andreas Orthey <andreas.orthey@gmx.de>
2018-08-17 18:06:37 +0900

    added 2dof manip in the plane;

---
+++ scripts/cspace_rigid_body_on_a_string_volume.py
@@ -0,0 +1,100 @@
+import sys
+import numpy as np
+from cspace_visualizer import *
+from cspace_rigid_body_on_a_string_generate_data import *
+from cspace_system_2dof import *
+import cspace_system_2dof as dof
+
+grey = '0.6'
+font_size = 45
+lim=5.0
+showSamples = True
+maxSamples = 50
+maxInfeasibleElements = 1000
+
+output_name = "rigid_body_on_a_string"
+
+qsname = output_name+"_qspace.png"
+csname = output_name+"_cspace.png"
+qsname_samples = output_name+"_qspace_volumes.png"
+csname_samples = output_name+"_cspace_volumes.png"
+
+qfname = "../data/samples/"+output_name+"/roadmap_level_0_robot_1.roadmap"
+cfname = "../data/samples/"+output_name+"/roadmap_level_0_robot_0.roadmap"
+
+generateQuotientSpaceDense(qfname, 1000)
+generateCSpaceDense(cfname, 60000)
+
+
+def plotQSPoints():
+    
+  Q = getPoints(qfname, maxSamples)
+  for q in Q:
+    x = float(q[0])
+    y = float(q[1])
+    feasible = q[3]
+    sufficient = q[4]
+    d = float(q[5])
+    if not feasible:
+      plt.axvline(x, color='k', linewidth=1)
+    elif sufficient:
+      plt.axvspan(x-d, x+d, alpha=0.5, hatch="/")
+    else:
+      delta=0.05
+      #plt.axvspan(x-d, x+d, alpha=0.5, fill=False, hatch="\\")
+      plt.fill([x-d, x+d, x+d, x-d], [-delta,-delta,+delta,+delta], fill=False, hatch='\\')
+
+def plotCSPoints():
+  Q = getPoints(cfname, maxSamples)
+  for q in Q:
+    x = float(q[0])
+    t = float(q[2])
+    feasible = q[3]
+    if feasible:
+      plt.plot(x,t,'o',color=grey, linewidth=1)
+    else:
+      plt.plot(x,t,'x',color='k', linewidth=1)
+
+def plotQuotientSpaceBackground():
+  P1 = np.load(open(r'tmp_QS_dense_1.npy', 'rb'))
+  P2 = np.load(open(r'tmp_QS_dense_2.npy', 'rb'))
+  plotCSpaceDelaunayGrey(P1,P2,0.15)
+
+def plotCSpaceBackground():
+  P1 = np.load(open(r'tmp_CS_dense_1.npy', 'rb'))
+  P2 = np.load(open(r'tmp_CS_dense_2.npy', 'rb'))
+  plotCSpaceDelaunayGrey(P1,P2,0.15)
+
+
+###############################################################################
+## QUOTIENT-SPACE
+###############################################################################
+fig = plt.figure(0)
+fig.patch.set_facecolor('white')
+ax = fig.gca()
+ax.set_xlabel(r'x',fontsize=font_size)
+ax.set_ylabel(r'\theta',rotation=1.57,fontsize=font_size)
+ax.tick_params(axis='both', which='major', pad=15)
+plt.axis([-lim,lim,-3.14,3.14])
+plotQuotientSpaceBackground()
+plt.savefig(qsname, bbox_inches='tight')
+if showSamples:
+  plotQSPoints()
+  plt.savefig(qsname_samples, bbox_inches='tight')
+
+###############################################################################
+## CSPACE
+###############################################################################
+fig = plt.figure(1)
+fig.patch.set_facecolor('white')
+ax = fig.gca()
+ax.set_xlabel(r'x',fontsize=font_size)
+ax.set_ylabel(r'\theta',rotation=1.57,fontsize=font_size)
+ax.tick_params(axis='both', which='major', pad=15)
+plt.axis([-lim,lim,-3.14,3.14])
+plotCSpaceBackground()
+plt.savefig(csname, bbox_inches='tight')
+if showSamples:
+  plotQSPoints()
+  plt.savefig(csname_samples, bbox_inches='tight')
+plt.show()
---
+++ scripts/urdf_create_planar_2dof_manip.py
@@ -0,0 +1,82 @@
+import os 
+import numpy as np
+from math import cos,sin,pi,atan2
+from urdf_create import *
+from urdf_create_primitives import *
+
+L1 = 1.5
+L2 = 1.5
+thickness = 0.2
+thickness_Z = 0.1
+
+robot_name = '2dof_manip'
+folder='robots/'
+fname = getPathname(folder, robot_name)
+
+f = open(fname,'w')
+f.write('<?xml version="1.0"?>\n')
+f.write('<robot name="'+robot_name+'">\n')
+
+hstr  = createRotatedCylinder("jlink1",0,0,0,0,0,1.57,thickness,thickness_Z)
+hstr += createCuboid("link1",0,L1/2+thickness,0,  2*thickness,L1,thickness_Z)
+hstr += createRotatedCylinder("jlink2",0,L1+2*thickness,0,0,0,1.57,thickness,thickness_Z)
+hstr += createCuboid("link2",0,L2/2+thickness,0,2*thickness,L2-1e-10,thickness_Z)
+
+hstr += createRevoluteJoint("joint_"+"jlink1"+"_"+"link1", "jlink1", "link1",0,0,0)
+hstr += createRigidJoint("joint_"+"link1"+"_"+"jlink2", "link1", "jlink2")
+hstr += createRevoluteJoint("joint_"+"jlink2"+"_"+"link2", "jlink2", "link2",0,L1+2*thickness,0)
+
+f.write(hstr)
+f.write('  <klampt package_root="../../.." default_acc_max="4" >\n')
+f.write('  </klampt>\n')
+f.write('</robot>')
+f.close()
+
+print "\nCreated new file >>",fname
+###############################################################################
+## Create inner 1dof robot
+###############################################################################
+inner_robot_name = robot_name + "_1dof_inner"
+fname = getPathname(folder, inner_robot_name)
+
+f = open(fname,'w')
+f.write('<?xml version="1.0"?>\n')
+f.write('<robot name="'+inner_robot_name+'">\n')
+
+hstr  = createRotatedCylinder("jlink1",0,0,0,0,0,1.57,thickness,thickness_Z*2)
+hstr += createCuboid("link1",0,L1/2+thickness,0,  2*thickness,L1,thickness_Z*2)
+hstr += createRotatedCylinder("jlink2",0,L1+2*thickness,0,0,0,1.57,thickness,thickness_Z*2)
+
+hstr += createRevoluteJoint("joint_"+"jlink1"+"_"+"link1", "jlink1", "link1",0,0,0)
+hstr += createRigidJoint("joint_"+"link1"+"_"+"jlink2", "link1", "jlink2")
+
+f.write(hstr)
+f.write('  <klampt package_root="../../.." default_acc_max="4" >\n')
+f.write('  </klampt>\n')
+f.write('</robot>')
+f.close()
+
+print "\nCreated new file >>",fname
+
+###############################################################################
+## Create outer 1dof robot
+###############################################################################
+outer_robot_name = robot_name + "_1dof_outer"
+fname = getPathname(folder, outer_robot_name)
+
+f = open(fname,'w')
+f.write('<?xml version="1.0"?>\n')
+f.write('<robot name="'+inner_robot_name+'">\n')
+
+hstr  = createRotatedCylinder("link1",0,0,0,0,0,1.57,thickness,thickness_Z/2)
+hstr += createRotatedCylinder("link2",0,L1+2*thickness,0,0,0,1.57,L2+thickness,thickness_Z/2)
+
+hstr += createRevoluteJoint("joint_"+"link1"+"_"+"link2", "link1", "link2")
+
+f.write(hstr)
+f.write('  <klampt package_root="../../.." default_acc_max="4" >\n')
+f.write('  </klampt>\n')
+f.write('</robot>')
+f.close()
+
+print "\nCreated new file >>",fname
