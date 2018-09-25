497040ef26147549e55a249b05f3a67f09150955
Andreas Orthey <andreas.orthey@gmx.de>
2018-09-25 17:08:46 +0900

    Refactor experiment section

---
+++ data/experiments/cspace_constant.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def SpecialEuclideanNeighborhood(rmax):
+  r1 = 1
+  r2 = rmax
+  rmin = np.min((r1, r2))
+  rmax = np.max((r1, r2))
+  k1 = 1.0/rmin
+  k2 = 1.0/rmax
+  c= k2*np.cos(0.5*np.pi - np.arctan(k1/k2))
+  return c
+
+print "PlanarLshape:",SpecialEuclideanNeighborhood(1.58902485821)
+print "Xshape:",SpecialEuclideanNeighborhood(1.70073513517)
