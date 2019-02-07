988989066d28025da5bc3451ed887f93b1b351fe
Andreas Orthey <andreas.orthey@gmx.de>
2019-02-07 16:20:14 +0100

    Fix test set for 2D bodies

--- scripts/test/all_experiments.py
+++ scripts/test/all_experiments.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import re
+import sys
 import numpy as np
 
 fname = "/home/aorthey/git/orthoklampt/build/"
@@ -21,9 +22,24 @@ def Execute(name, N, max_time):
       times.append(t)
   times = np.array(times)
   sN = len(times[times < max_time])
-  print name,":",sN,"/",N," successful. Times: (Average:",np.mean(times), "Worst:",np.max(times), "Max:", max_time,")"
+  if sN < N:
+    sys.stdout.write('*FAILED |')
+  else:
+    sys.stdout.write('    OK  |')
 
+  print name,":",sN,"/",N," successful. Times: (Average:",np.mean(times), "Worst:",np.max(times), "Max:", max_time,")"
+  return [sN>=N,sN,N,np.mean(times)]
+
+
+
+print '#'*80
+print "Test of 2D Rigid Bodies"
+print '#'*80
 Execute("02D_disk.xml", 5, 1.0)
+Execute("02D_disk_bugtrap.xml", 5, 0.2)
+Execute("02D_disk_narrow.xml", 5, 1.0)
+Execute("02D_disk_decomposed.xml", 5, 1.0)
 Execute("03D_corner.xml", 5, 0.5)
 Execute("03D_misleading.xml", 5, 0.5)
 Execute("03D_nonsimple.xml", 5, 2.0)
+print '#'*80
