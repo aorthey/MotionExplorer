8381b25dcac33c95a8af33fe5364fbdfefdb61de
Andreas Orthey <andreas.orthey@gmx.de>
2019-02-07 16:10:43 +0100

    Add two startup files: one with GUI, one without.

---
+++ test/planner_standalone.cpp
@@ -0,0 +1,21 @@
+#include "gui/gui_planner.h"
+#include "environment_loader.h"
+#include "planner/planner.h"
+
+int main(int argc, char **argv) {
+
+  EnvironmentLoader env = EnvironmentLoader::from_args(argc, argv);
+
+  strncpy(argv[0], std::string(80,'#').c_str(), strlen(argv[0]));
+  for(int i = 1; i < argc; i++) memset(argv[i], 0, strlen(argv[i]));
+
+  PlannerMultiInput in = env.GetPlannerInput();
+
+  GLUIPlannerGUI gui(env.GetBackendPtr(),env.GetWorldPtr());
+  gui.AddPlannerInput(in);
+  gui.SetWindowTitle("MotionPlannerVisualizer");
+  if(in.inputs.empty()) env.GetBackendPtr()->state("draw_robot").active = 1;
+  gui.backend->OnCommand("planner_advance_until_solution","");
+
+  return 0;
+}
