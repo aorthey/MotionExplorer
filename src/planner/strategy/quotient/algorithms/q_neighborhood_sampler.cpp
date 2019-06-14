fbe51efad8a6c6392c3eaf274a335320bdcc4afe
Andreas Orthey <andreas.orthey@gmx.de>
2019-06-14 17:59:42 +0200

    Put nbh sampler into own class

---
+++ src/planner/strategy/quotient/algorithms/q_neighborhood_sampler.cpp
@@ -0,0 +1,34 @@
+#include "common.h"
+#include "gui/common.h"
+#include "q_neighborhood_sampler.h"
+
+using namespace ompl::geometric;
+
+//############################################################################
+// Setup
+//############################################################################
+QNeighborhoodSampler::QNeighborhoodSampler(const base::SpaceInformationPtr &si, Quotient *parent ): BaseT(si, parent)
+{
+  setName("QNeighborhoodSampler"+std::to_string(id));
+}
+
+//############################################################################
+// Grow Functions
+//############################################################################
+void QNeighborhoodSampler::Grow(double t)
+{
+  if(firstRun){
+    firstRun = false;
+    Init();
+  }
+  Configuration *q = new Configuration(Q1);
+  Q1_sampler->sampleUniform(q->state);
+  q->SetRadius(DistanceInnerRobotToObstacle(q->state));
+  if(q->GetRadius()>minimum_neighborhood_radius){
+    AddConfigurationToCoverGraph(q);
+  }
+}
+bool QNeighborhoodSampler::GetSolution(ob::PathPtr &solution)
+{
+  return false;
+}
---
+++ src/planner/strategy/quotient/algorithms/q_neighborhood_sampler.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "planner/strategy/quotient/quotient_cover_queue.h"
+
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+namespace ompl
+{
+  namespace geometric
+  {
+
+    //QNeighborhoodSampler: just uniformly sample space, do not search for a path (for
+    //visualization purposes)
+    class QNeighborhoodSampler: public og::QuotientCoverQueue
+    {
+      typedef og::QuotientCoverQueue BaseT;
+    public:
+
+      QNeighborhoodSampler(const ob::SpaceInformationPtr &si, Quotient *parent = nullptr);
+      ~QNeighborhoodSampler(void) = default;
+
+      virtual bool GetSolution(ob::PathPtr &solution) override;
+
+      virtual void Grow(double t) override;
+    };
+  }
+}
+
+
